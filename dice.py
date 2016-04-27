3b07e8d919842951f67b868fd4d7d84fc34a267c
tgi-f <tgi-f@yandex.ru>
2016-04-27 08:07:45 +0500

    Add command line arguments

--- dice.py
+++ dice.py
@@ -1,8 +1,29 @@
 #!/usr/bin/env python3
 
+import argparse
+
+
+def entry():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-n', help='the loop length (default 3)', dest='loop',
+        action='store', default=3, type=int, choices=(1, 2, 3))
+    parser.add_argument(
+        '-p',
+        nargs='+',
+        help='participants, e.g. Name1 Name2 ... NameN',
+        dest='play',
+        action='store', required=True)
+
+    return parser.parse_args()
+
 
 def main():
-    print('Hello, World!')
+    args = entry()
+    print('Количество бросков кубика: {}'.format(args.loop))
+    print('Игроки:')
+    for i in args.play:
+        print('\t', i)
 
 
 if __name__ == '__main__':
